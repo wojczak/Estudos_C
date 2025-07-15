63382f2c707edf763bd23e47ab7a6e4d439a4262
Daniel Wojczak <wojczak33@gmail.com>
2025-07-15 17:57:00 -0300

    Criado o projeto Calculadora

---
+++ 2_Calculadora/src/main.c
@@ -0,0 +1,92 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+void soma(int a, int b);
+void subtrai(int a, int b);
+void multipica(int a, int b);
+void divide(int a, int b);
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int opcao = 0;
+
+    do
+    {
+        puts("Digite a operacao desejada: ");
+        puts("1 - Soma\n"
+             "2 - Subtracao\n"
+             "3 - Multiplicacao\n"
+             "4 - Divisao\n"
+             "5 - Sair\n");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            soma(0, 0);
+            break;
+        case 2:
+            subtrai(0, 0);
+            break;
+        case 3:
+            multipica(0, 0);
+            break;
+        case 4:
+            divide(0, 0);
+            break;
+        case 5:
+            puts("Saindo...");
+            break;
+        default:
+            puts("Opcao Invalida! Tente Novamente.");
+            continue;
+        }
+
+    } while (1);
+
+    return 0;
+}
+
+void soma(int a, int b)
+{
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
+    printf("A soma de %d + %d e %d\n", a, b, a + b);
+}
+
+void subtrai(int a, int b)
+{
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
+    printf("A subtracao de %d - %d e %d\n", a, b, a - b);
+}
+
+void multipica(int a, int b)
+{
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
+    printf("A multiplicacao de %d * %d e %d\n", a, b, a * b);
+}
+void divide(int a, int b)
+{
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
+    if (b == 0)
+    {
+        puts("Divisao por zero nao e permitida.");
+        return;
+    }
+    printf("A divisao de %d / %d e %d\n", a, b, a / b);
+}
